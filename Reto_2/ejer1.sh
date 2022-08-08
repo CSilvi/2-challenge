3d4fb5b13a6f4321afc78e2bd50b7b2d8962e9f2
Silvina Carlos <72581867+CSilvi@users.noreply.github.com>
2022-08-08 19:39:43 -0300

    Se agrego comentario del archiv8

--- Reto_2/ejer1.sh
+++ Reto_2/ejer1.sh
@@ -2,6 +2,8 @@
 #!/bin/bash 
 
 
+
  touch archiv{1..9}
  chmod a-rwx archiv*
 
+# Cabe mencionar que el archi8 no se lo pudo agregar al repositorio. Esto se debe a que se le quito los permisos (x,r,w) al usuario que lo creo. 
